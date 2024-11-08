5a596a931eb5875f63b5885ba0136c591b7ab2dc
MartaGuzman <marta.guzmanalarcon@gmail.com>
2024-11-08 13:24:08 +0100

    connectionPatient done received ddata deleted

--- AppClientDoctor_TelemonitoringCHF/src/main/java/ConnectionDoctor/ConnectionDoctor.java
+++ AppClientDoctor_TelemonitoringCHF/src/main/java/ConnectionDoctor/ConnectionDoctor.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package ConexionDoctor;
+package ConnectionDoctor;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.net.Socket;
