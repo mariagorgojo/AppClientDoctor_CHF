f9ced902a50e9384e993e6fadf326d537c82dd8b
maria <maria@192.168.1.103>
2024-11-08 13:25:08 +0100

    Utilities package

--- AppClientDoctor_TelemonitoringCHF/src/main/java/Utilities/InputOutput.java
+++ AppClientDoctor_TelemonitoringCHF/src/main/java/Utilities/InputOutput.java
@@ -4,7 +4,56 @@
  */
 package Utilities;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class InputOutput {
+    
+    private static BufferedReader r = new BufferedReader(new InputStreamReader(System.in));
+    
+    // Read console inputs 
+    		public static int readInteger() {
+	        int num = 0;
+	        boolean ok = false;
+	        do {
+	            try {
+	            	num = Integer.parseInt(r.readLine());
+	                if (num < 0) {
+	                    ok = false;
+	                    System.out.print("You didn't type a valid number!");
+	                } else {
+	                    ok = true;
+	                }
+	            } catch (IOException e) {
+	                e.getMessage();
+	            } catch (NumberFormatException nfe) {
+	            	System.out.print("You didn't type a valid number!");
+	            }
+	        } while (!ok);
+
+	        return num;
+	    }
+
+		
+		public static String readString() {
+	        String text = null;
+	        boolean ok = false;
+	        do {
+	            try {
+	            	text = r.readLine();
+	                if (!text.isEmpty()) {
+	                    ok = true;
+	                } else {
+	                    System.out.println("Empty string, please try again:");
+	                }
+	            } catch (IOException e) {
+
+	            }
+	        } while (!ok);
+
+	        return text;
+	    }
+    
     
 }
