1049dd38d1e96dbcb3ee28dfb76c52984f4f2988
mariagorgojo <maria.gorgojogarcia@usp.ceu.es>
2024-12-08 12:53:39 +0100

    patient pojo doctorapp

--- AppClientDoctor_TelemonitoringCHF/src/main/java/pojos/Patient.java
+++ AppClientDoctor_TelemonitoringCHF/src/main/java/pojos/Patient.java
@@ -20,6 +20,7 @@ public class Patient {
     private Integer phoneNumber;
     private LocalDate dob;
     private Doctor doctor;
+    private ArrayList<Disease> previousDiseases;
     private ArrayList<Episode> episodes;
 
     //constructor for registration
@@ -33,6 +34,7 @@ public class Patient {
         this.gender = gender;
         this.phoneNumber = phoneNumber;
         this.dob = dob;
+        previousDiseases = new ArrayList<>();
         episodes = new ArrayList<>();
     }
 
@@ -50,6 +52,7 @@ public class Patient {
         this.phoneNumber = phoneNumber;
         this.dob = dob;
         this.doctor = doctor;
+        previousDiseases = new ArrayList<>();
         episodes = new ArrayList<>();
     }
     
@@ -135,6 +138,14 @@ public class Patient {
         return doctor;
     }
 
+    public ArrayList<Disease> getPreviousDiseases() {
+        return previousDiseases;
+    }
+
+    public void setPreviousDiseases(ArrayList<Disease> previousDiseases) {
+        this.previousDiseases = previousDiseases;
+    }
+    
     public ArrayList<Episode> getEpisodes() {
         return episodes;
     }
@@ -145,7 +156,7 @@ public class Patient {
 
     @Override
     public String toString() {
-        return "Patient{" + "dni=" + dni + ", name=" + name + ", surname=" + surname + ", email=" + email + ", gender=" + gender + ", phoneNumber=" + phoneNumber + ", dob=" + dob + ", doctor=" + doctor + ", episodes=" + episodes + '}';
+        return "Patient{" + "dni=" + dni + ", name=" + name + ", surname=" + surname + ", email=" + email + ", gender=" + gender + ", phoneNumber=" + phoneNumber + ", dob=" + dob + ", doctor=" + doctor  + ", previous diseases=" + previousDiseases + ", episodes=" + episodes + '}';
     }
 
     public enum Gender {
